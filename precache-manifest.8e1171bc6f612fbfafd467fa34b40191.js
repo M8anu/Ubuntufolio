655788572014cbc6a2611b4f27efeeedf5781319
Manuel <m8anuworcc@gmail.com>
2021-06-02 21:55:04 +0200

    Updates

---
+++ precache-manifest.8e1171bc6f612fbfafd467fa34b40191.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2df7e78dfb7a50025aef4dcb6e10692",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db8e0cd2ea40ba8d7c79",
+    "url": "/static/css/main.54336e92.chunk.css"
+  },
+  {
+    "revision": "60e21bd30b9f64aa2d9d",
+    "url": "/static/js/2.d4e49c4a.chunk.js"
+  },
+  {
+    "revision": "db8e0cd2ea40ba8d7c79",
+    "url": "/static/js/main.1b3be6a0.chunk.js"
+  },
+  {
+    "revision": "5d3ab7dbb04846bf332e",
+    "url": "/static/js/runtime~main.128a37bf.js"
+  },
+  {
+    "revision": "5f8b3fa3ce343c1c6b42203c08d8cf9b",
+    "url": "/static/media/Logogrande.5f8b3fa3.jpg"
+  },
+  {
+    "revision": "d4557c470561cc51f7ebaebe632ebd5c",
+    "url": "/static/media/M8anu.d4557c47.jpg"
+  }
+]);
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import OS from "./OS";
-
-ReactDOM.render(<OS />, document.getElementById("root"));

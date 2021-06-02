266033ab408d2cd99eef885091f00ca1bacb1a2a
Manuel <m8anuworcc@gmail.com>
2021-06-02 22:24:39 +0200

    avanzando 99% grasias @Melchor629

--- src/content/Help.jsx
+++ src/content/Help.jsx
@@ -4,7 +4,7 @@ import "./Help.css";
 export default function Help() {
   return (
     <div className="HelpBody">
-      <iframe src="https://m8anu.github.io/Consolefolio/" style={{border:"0px #f5f6f8 solid", height: "calc(80vh - 25px)"}}
+      <iframe src="https://m8anu.github.io/Consolefolio/" style={{border:"0px #f5f6f8 solid", height: "transform: scaleY(1.006) translateY(2px)"}}
        name="Consolefolio" scrolling="no" frameborder="0" marginheight="0px" 
        marginwidth="0px"  width="100%" allowfullscreen></iframe>
     </div>

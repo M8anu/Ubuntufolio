30d1b6b95e4946aae00bcfd3782c514edd5cd510
Manuel <m8anuworcc@gmail.com>
2021-06-03 08:48:32 +0200

    scroll

--- src/content/Help.jsx
+++ src/content/Help.jsx
@@ -5,7 +5,7 @@ export default function Help() {
   return (
     <div className="HelpBody">
       <iframe src="https://m8anu.github.io/Consolefolio/" style={{border:"0px #f5f6f8 solid", height:"calc(80vh - 23px)"}}
-       name="Consolefolio" scrolling="no" frameborder="0" marginheight="0px" 
+       name="Consolefolio" scrolling="yes" frameborder="0" marginheight="0px" 
        marginwidth="0px"  width="100%" allowfullscreen></iframe>
     </div>
   );
